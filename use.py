a2f348804f2d0351e98d164e298466ad8e8c4d0e
Danil <96177948+0x7o@users.noreply.github.com>
2022-01-26 15:57:02 +0800

    Create use.py

---
+++ use.py
@@ -0,0 +1,19 @@
+import numpy as np
+import torch
+import random
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+model_name = '0x7194633/gameGPT-large'
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+model = GPT2LMHeadModel.from_pretrained(model_name)
+
+if torch.cuda.is_available():
+  model.cuda()
+  
+def generate(text, **kwargs):
+    inpt = tokenizer.encode(text, return_tensors="pt")
+    out = model.generate(inpt.cuda(), **kwargs)
+    return tokenizer.decode(out[0])
+  
+act = "Тест"
+print(generate(act, max_length=500, repetition_penalty=5.0, top_k=5, top_p=0.95, temperature=0.9))
