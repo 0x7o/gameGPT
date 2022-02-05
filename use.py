ae3b87ec6b4bca85407fa76c582a67d12212eb0c
Danil <96177948+0x7o@users.noreply.github.com>
2022-02-05 14:12:12 +0800

    Update use.py

--- use.py
+++ use.py
@@ -1,7 +1,7 @@
 import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-model_name = '0x7194633/gameGPT-large'
+model_name = '0x7194633/gameGPT'
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 model = GPT2LMHeadModel.from_pretrained(model_name)
 
